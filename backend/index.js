54b5ba5750cde797fc5f1eddd2ac1d5057129a1f
Mohd Ajmal Raza <mohdajmalraza92786@gmail.com>
2025-11-24 22:59:57 +0530

    feat: add product details route (/api/products/:productId)

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ async function getAllProducts() {
   }
 }
 
-// Route to get all products
+// Route to get all products from the db
 app.get("/api/products", async (req, res) => {
   try {
     const products = await getAllProducts();
@@ -34,6 +34,42 @@ app.get("/api/products", async (req, res) => {
     return res.status(500).json({
       success: false,
       message: "Error in fetching products",
+      error: error.message,
+    });
+  }
+});
+
+async function getProductById(productId) {
+  try {
+    return await Product.findById(productId);
+  } catch (error) {
+    throw new Error("Error fetching product by id: " + error.message);
+  }
+}
+
+// Route to gets product by productId from the db
+app.get("/api/products/:productId", async (req, res) => {
+  try {
+    const { productId } = req.params;
+    if (!productId) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Product ID is required" });
+    }
+
+    const product = await getProductById(productId);
+
+    if (product) {
+      return res.status(200).json({ success: true, data: { product } });
+    } else {
+      return res
+        .status(404)
+        .json({ success: false, message: "Product not found." });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Error in fetching product by id",
       error: error.message,
     });
   }
